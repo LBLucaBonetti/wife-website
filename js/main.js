07982786e39373654847521039fe0fb94c99552e
Luca <lb.luca.bonetti@gmail.com>
2023-08-22 21:12:54 +0200

    feat(layout): add services section skeleton, solid navbar behavior and rename min-height class

--- js/main.js
+++ js/main.js
@@ -4,13 +4,21 @@
 
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-
 //
-// Place any custom JS here
+// Custom JS here
 //
+const solidNavbarClassName = "nav-scrolled-down";
+const yPositionForSolidNavbar = 60;
+const nav = document.getElementById("navbar");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= yPositionForSolidNavbar) {
+    nav.classList.add(solidNavbarClassName);
+  } else if (window.scrollY < yPositionForSolidNavbar) {
+    nav.classList.remove(solidNavbarClassName);
+  }
+});
 
 // Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new bootstrap.Popover(popover)
-  })
+document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
+  new bootstrap.Popover(popover);
+});
