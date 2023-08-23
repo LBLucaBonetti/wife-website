175eea848b7fc0be4d62fa7c6a8ce5cb9b9908cf
Luca <lb.luca.bonetti@gmail.com>
2023-08-23 12:31:10 +0200

    feat(home-background): add responsive home background jpg images

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,24 @@ window.addEventListener("scroll", () => {
   }
 });
 
+import home from "../img/home.jpg";
+import homeMd from "../img/home-md.jpg";
+import homeSm from "../img/home-sm.jpg";
+function changeHomeImg() {
+  const homeImgSrcJpg = document.getElementById("home-img-src-jpg");
+  const homeImg = document.getElementById("home-img");
+  let imgToSet = homeSm;
+  const width = window.innerWidth;
+  if (width >= 576 && width <= 1024) {
+    imgToSet = homeMd;
+  } else if (width > 1024) {
+    imgToSet = home;
+  }
+  homeImgSrcJpg.srcset = imgToSet;
+  homeImg.src = imgToSet;
+}
+changeHomeImg();
+window.addEventListener("resize", changeHomeImg);
 import marikasilvestri from "../img/marikasilvestri.jpg";
 const aboutImgSrcJpg = document.getElementById("about-img-src-jpg");
 aboutImgSrcJpg.srcset = marikasilvestri;
