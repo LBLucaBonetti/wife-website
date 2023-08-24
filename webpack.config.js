ae76717834e4da7184c717ccdef28416c0373896
Luca <lb.luca.bonetti@gmail.com>
2023-08-24 16:09:28 +0200

    feat(bootstrap-shrink): add purgecss to further minify the output

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const path = require("path");
-const autoprefixer = require("autoprefixer");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -40,11 +39,6 @@ module.exports = {
           {
             // Loader for webpack to process CSS with PostCSS
             loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [autoprefixer],
-              },
-            },
           },
           {
             // Loads a SASS/SCSS file and compiles it to CSS
